946e241e1516aa6393e36cc204ff4a5851963c18
Adan <adanvitor90@gmail.com>
2021-05-19 18:02:53 -0300

    Version 1.0.0

--- Sources/AutolayoutExtensions/AutolayoutExtensions.swift
+++ Sources/AutolayoutExtensions/AutolayoutExtensions.swift
@@ -1,3 +1,109 @@
-struct AutolayoutExtensions {
-    var text = "Hello, World!"
+import Foundation
+import UIKit
+
+//https://www.avanderlee.com/swift/auto-layout-programmatically/
+@propertyWrapper
+public struct UsesAutoLayout<T: UIView> {
+    public var wrappedValue: T {
+        didSet {
+            wrappedValue.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+
+    public init(wrappedValue: T) {
+        self.wrappedValue = wrappedValue
+        wrappedValue.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UIView{
+    
+    func constraintsToSameWidth(view: UIView,
+                                multiplier: CGFloat = 1) -> [NSLayoutConstraint]{
+        return [widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: multiplier)]
+    }
+    
+    func constraintsToSameHeight(view: UIView,
+                                 multiplier: CGFloat = 1) -> [NSLayoutConstraint]{
+        return [heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplier)]
+    }
+    
+    func constraintsToSameSize(view: UIView,
+                               widthMultiplier: CGFloat = 1,
+                               heightMultiplier: CGFloat = 1) -> [NSLayoutConstraint]{
+        return [widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: widthMultiplier),
+            heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: heightMultiplier)]
+    }
+    
+    func constraintsForCenterIn(view: UIView) -> [NSLayoutConstraint]{
+        return [
+            centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+    }
+    
+    func constraintsForAnchoringTo(view : UIView,
+                                   top : CGFloat = 0,
+                                   leading : CGFloat = 0,
+                                   bottom : CGFloat = 0,
+                                   trailing : CGFloat = 0) -> [NSLayoutConstraint] {
+        
+        return [
+            topAnchor.constraint(equalTo: view.topAnchor, constant: top),
+            leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leading),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -1*bottom),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor , constant: -1*trailing),
+        ]
+    }
+    
+    func constraintsForAnchoringTo(view : UIView,
+                                   padding : CGFloat) -> [NSLayoutConstraint] {
+        
+        return constraintsForAnchoringTo(view: view,
+                                         top: padding,
+                                         leading: padding,
+                                         bottom: padding,
+                                         trailing: padding)
+    }
+    
+    func constraintsForAnchoringToSafeArea(view: UIView,
+                                   top : CGFloat = 0,
+                                   leading : CGFloat = 0,
+                                   bottom : CGFloat = 0,
+                                   trailing : CGFloat = 0) -> [NSLayoutConstraint]{
+        return [
+            topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: top),
+            leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leading),
+            bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -1*bottom),
+            trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor , constant: -1*trailing),
+        ]
+    }
+    
+    func constraintsForAnchoringToSafeArea(view: UIView,
+                                           padding : CGFloat) -> [NSLayoutConstraint]{
+        return constraintsForAnchoringToSafeArea(view: view,
+                                                 top: padding,
+                                                 leading: padding,
+                                                 bottom: padding,
+                                                 trailing: padding)
+    }
+    
+    
+
+}
+
+extension NSLayoutConstraint{
+    func activate(){
+        self.isActive = true
+    }
+    
+    func setLessPriorityThan(otherConstraint : NSLayoutConstraint){
+        self.priority = UILayoutPriority(max(otherConstraint.priority.rawValue - 10.0,0))
+    }
+}
+
+extension Array where Element == NSLayoutConstraint{
+    func activate(){
+        self.forEach{constraint in constraint.activate()}
+    }
 }
