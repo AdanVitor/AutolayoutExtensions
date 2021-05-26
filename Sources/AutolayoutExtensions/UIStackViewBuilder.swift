7793ad955685246f389fbcd970d2a22a8fdd6553
Adan <adanvitor90@gmail.com>
2021-05-26 18:14:15 -0300

    Create UiStackViewBuilder

---
+++ Sources/AutolayoutExtensions/UIStackViewBuilder.swift
@@ -0,0 +1,50 @@
+//
+//  UIStackViewBuilder.swift
+//  Dicee-iOS13
+//
+//  Created by Adan on 20/05/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIStackView{
+    func addArrangedSubviews(views : UIView...){
+        views.forEach{
+            self.addArrangedSubview($0)
+        }
+    }
+    
+    func applyPadding(top: CGFloat = 0, left : CGFloat = 0, bottom: CGFloat = 0, right: CGFloat = 0){
+        self.layoutMargins = UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+        self.isLayoutMarginsRelativeArrangement = true
+    }
+}
+
+class UIStackViewBuilder{
+    let stackView = UIStackView()
+    
+    init(axis : NSLayoutConstraint.Axis){
+        stackView.axis = axis
+    }
+    
+    func withSpacing(_ spacing : CGFloat) -> Self{
+        stackView.spacing = spacing
+        return self
+    }
+    
+    func withAligment(_ aligment : UIStackView.Alignment) -> Self{
+        stackView.alignment = aligment
+        return self
+    }
+    
+    func withDistribution(_ distribution : UIStackView.Distribution) -> Self{
+        stackView.distribution = distribution
+        return self
+    }
+    
+    
+    func build() -> UIStackView{
+        return stackView
+    }
+}
