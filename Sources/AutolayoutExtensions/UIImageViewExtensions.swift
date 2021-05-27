464d38a4282294dad8e0eaba3fc9ad18dd10191f
Adan <adanvitor90@gmail.com>
2021-05-27 06:24:05 -0300

    Adding then closure to UIImageView

--- Sources/AutolayoutExtensions/UIImageViewExtensions.swift
+++ Sources/AutolayoutExtensions/UIImageViewExtensions.swift
@@ -10,15 +10,18 @@ import UIKit
 
 public extension UIImageView{
     static func createImageView(image : UIImage?,
-                                contentMode : UIView.ContentMode = .scaleAspectFit) -> UIImageView{
+                                contentMode : UIView.ContentMode = .scaleAspectFit,
+                                then : ((UIImageView) -> Void)? = nil) -> UIImageView{
         let imageView = UIImageView()
         imageView.image = image
         imageView.contentMode = contentMode
+        then?(imageView)
         return imageView
     }
     
     static func createImageView(imageName : String,
-                                contentMode : UIView.ContentMode = .scaleAspectFit) -> UIImageView{
-        return createImageView(image: UIImage(named: imageName), contentMode: contentMode)
+                                contentMode : UIView.ContentMode = .scaleAspectFit,
+                                then : ((UIImageView) -> Void)? = nil) -> UIImageView{
+        return createImageView(image: UIImage(named: imageName), contentMode: contentMode, then: then)
     }
 }
