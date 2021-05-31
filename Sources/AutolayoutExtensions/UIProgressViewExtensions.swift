ac02902306d5e6a4a19b30632c99dc6f29db7d05
Adan <adanvitor90@gmail.com>
2021-05-31 08:13:33 -0300

    Adding create function to UIProgressView

---
+++ Sources/AutolayoutExtensions/UIProgressViewExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adan on 31/05/21.
+//
+
+import Foundation
+import UIKit
+
+public extension UIProgressView{
+    
+    static func create(then : (UIProgressView) -> Void) -> UIProgressView{
+        let progressView = UIProgressView()
+        then(progressView)
+        return progressView
+        
+    }
+}
