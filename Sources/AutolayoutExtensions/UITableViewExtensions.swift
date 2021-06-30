b04f3dd64797a319091cc8d2d83bfa94f2830ca7
Adan <adanvitor90@gmail.com>
2021-06-30 04:23:56 -0300

    Adding extensions scrollToBottom and scrollToTop to table view

--- Sources/AutolayoutExtensions/UITableViewExtensions.swift
+++ Sources/AutolayoutExtensions/UITableViewExtensions.swift
@@ -23,3 +23,32 @@ public extension CellIdentifiable where Self: UITableViewCell{
         tableView.register(Self.self, forCellReuseIdentifier: identifier)
     }
 }
+//https://stackoverflow.com/questions/33705371/how-to-scroll-to-the-exact-end-of-the-uitableview
+public extension UITableView {
+
+    func scrollToBottom(){
+
+        DispatchQueue.main.async {
+            let indexPath = IndexPath(
+                row: self.numberOfRows(inSection:  self.numberOfSections-1) - 1,
+                section: self.numberOfSections - 1)
+            if self.hasRowAtIndexPath(indexPath) {
+                self.scrollToRow(at: indexPath, at: .bottom, animated: true)
+            }
+        }
+    }
+
+    func scrollToTop() {
+
+        DispatchQueue.main.async {
+            let indexPath = IndexPath(row: 0, section: 0)
+            if self.hasRowAtIndexPath(indexPath) {
+                self.scrollToRow(at: indexPath, at: .top, animated: false)
+           }
+        }
+    }
+
+    func hasRowAtIndexPath(_ indexPath: IndexPath) -> Bool {
+        return indexPath.section < self.numberOfSections && indexPath.row < self.numberOfRows(inSection: indexPath.section)
+    }
+}
